8facd057a0e10b2578c4a846d3b28c3e6a65e6c8
unknown <vuonghk1@gmail.com>
2021-04-07 23:39:10 +0700

    Add headerContent File

---
+++ Modules/Admin/Resources/views/components/headerContent.blade.php
@@ -0,0 +1,15 @@
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0">{{ $name }}</h1>
+            </div><!-- /.col -->
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">{{ $name }}</a></li>
+                    <li class="breadcrumb-item active"><a href="#">{{ $key }}</a></li>
+                </ol>
+            </div>
+        </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+</div>
