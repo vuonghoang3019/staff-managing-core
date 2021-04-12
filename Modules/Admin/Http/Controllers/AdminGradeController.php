6f45a8dec1b0b54a0f1bfe48654029d59adb9e9e
unknown <vuonghk1@gmail.com>
2021-04-12 21:09:21 +0700

    Add file

---
+++ Modules/Admin/Http/Controllers/AdminGradeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\Admin\Http\Controllers;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class AdminGradeController extends Controller
+{
+
+}
