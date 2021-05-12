bc2582e08cfd994aedb8e59bf821d25ff2c154cb
unknown <vuonghk1@gmail.com>
2021-05-12 11:15:12 +0700

    complete view calendar

--- Modules/Admin/Services/CalendarService.php
+++ Modules/Admin/Services/CalendarService.php
@@ -3,6 +3,7 @@
 namespace Modules\Admin\Services;
 
 use App\Models\Schedule;
+use Illuminate\Support\Facades\DB;
 
 class CalendarService
 {
@@ -10,29 +11,23 @@ class CalendarService
     {
         $calendarData = [];
         $timeRange = (new TimeService)->generateTimeRange(config('app.calendar.start'), config('app.calendar.end'));
-        $schedules = Schedule::with('calendar', 'teacher', 'class', 'course')->get();
-//        dd($schedules);
-        foreach ($timeRange as $time)
-        {
+        $schedules = Schedule::with('teacher','calendar','course','class')->get();
+        foreach ($timeRange as $time) {
             $timeText = $time['start'] . ' - ' . $time['end'];
             $calendarData[$timeText] = [];
             foreach ($weekDays as $index => $day) {
-                $schedule = $schedules->where('calendar.day', $index)->where('start_time', $time['start'])->first();
-
+                $schedule = $schedules->where('calendar.day', $day)
+                    ->where('calendar.start_time','>',$time['start'])->first();
                 if ($schedule) {
                     array_push($calendarData[$timeText], [
-                        'className'   => $schedules->class->name,
-                        'teacherName' => $schedules->teacher->name,
-                        'rowspan'     => $schedules->difference / 30 ?? ''
+                        'className'   => $schedule->class->name,
+                        'teacherName' => $schedule->teacher->name,
+                        'rowspan'     => $schedule->difference / 30 ?? ''
                     ]);
-                }
-                else if (!$schedules->where('day',$index)->where('start_time','<',$time['start'])->where('end_time','>=',$time['end'])->count())
-                {
-                    array_push($calendarData[$timeText],1);
-                }
-                else
-                {
-                    array_push($calendarData[$timeText],0);
+                } else if (!$schedules->where('calendar.day', $day)->where('calendar.start_time', '<', $time['start'])->where('calendar.end_time', '>=', $time['end'])->count()) {
+                    array_push($calendarData[$timeText], 1);
+                } else {
+                    array_push($calendarData[$timeText], 0);
                 }
             }
         }
