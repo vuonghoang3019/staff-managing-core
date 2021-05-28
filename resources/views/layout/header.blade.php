5a22b1d98259e0dd6050a6e81c59695a60f586d1
unknown <vuonghk1@gmail.com>
2021-05-29 00:01:48 +0700

    add view home

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,49 @@
+{{--<div id="preloader">--}}
+{{--    <div class="loader-container">--}}
+{{--        <div class="progress-br float shadow">--}}
+{{--            <div class="progress__item"></div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
+<!-- END LOADER -->
+<!-- Start header -->
+<header class="top-navbar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.html">
+                <img src="" alt=""/>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-host"
+                    aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbars-host">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="about.html">About Us</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Course </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown-a">
+                            <a class="dropdown-item" href="course-grid-2.html">Course Grid 2 </a>
+                            <a class="dropdown-item" href="course-grid-3.html">Course Grid 3 </a>
+                            <a class="dropdown-item" href="course-grid-4.html">Course Grid 4 </a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Blog </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown-a">
+                            <a class="dropdown-item" href="blog.html">Blog </a>
+                            <a class="dropdown-item" href="blog-single.html">Blog single </a>
+                        </div>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="teachers.html">Teachers</a></li>
+                    <li class="nav-item"><a class="nav-link" href="pricing.html">Pricing</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+<!-- End header -->
---
+++ resources/views/master/master.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!-- Basic -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Mobile Metas -->
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Site Metas -->
+    @yield('title')
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="{{ asset('home/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/css/versions.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/css/responsive.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/css/custom.css') }}">
+    <link rel="stylesheet" href="{{ asset('home/fontawesome-free/css/all.css') }}">
+    @yield('css')
+</head>
+<body class="host_version">
+<!-- LOADER -->
+@include('layout.header')
+
+@yield('content')
+
+@include('layout.footer')
+</body>
+<script src="{{ asset('home/js/modernizer.js') }}"></script>
+<script src="{{ asset('home/js/all.js') }}"></script>
+<script src="{{ asset('home/js/custom.js') }}"></script>
+<script src="{{ asset('home/js/timeline.min.js') }}"></script>
+<script>
+    timeline(document.querySelectorAll('.timeline'), {
+        forceVerticalMode: 700,
+        mode: 'horizontal',
+        verticalStartPosition: 'left',
+        visibleItems: 4
+    });
+</script>
+</html>
